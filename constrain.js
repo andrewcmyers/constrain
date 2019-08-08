585d5b1fbffe4d94e2eb2e4ff0fe269f706e124b
Andrew Myers <andru@cs.cornell.edu>
2019-08-08 10:42:08 -0400

    More beautification

--- constrain.js
+++ constrain.js
@@ -827,26 +827,25 @@ function isFigure(figure) {
     return (figure.connector !== undefined)
 }
 
-// Adapted from numeric-1.2.6.js to allow f to supply the gradient directly.
+// Adapted from numeric-1.2.6.js to allow f to supply the gradient directly. Uses
+// various functions from that package.
 //
-// Returns: a local minimum of a function f(x) whose gradient is g(x).
+// Returns: a local minimum of a function f(x) whose gradient is g(x), using
+//   a gradient descent search.
 // Requires: fg(x) or fg(x, false) must return just f(x),
 //   fg(x, true) must return [ f(x), (grad f)(x)] or [f(x0), undefined]. In the former case,
 //   (grad f) must return an array whose length is the same as x. In the latter
 //   case, a numeric gradient is computed using f, probably more expensively.
+//   If the computed gradient is infinite or contains NaN, the search terminates.
+// Effects: if callback is a function, it is invoked on each iteration of the algorithm.
+//    If it returns true, the search for the local minimum halts.
 function uncmin(fg, x0, tol, maxit, callback, options) {
     const grad = numeric.gradient
-    if (typeof options === "undefined") {
-        options = {}
-    }
-    if (typeof tol === "undefined") {
-        tol = 1e-8
-    }
+    if (options === undefined) options = {}
+    if (tol === undefined) tol = 1e-8
+    if (maxit === undefined) maxit = 1000
 
-    function gradient(x) {
-        return grad(fg, x)
-    }
-    if (typeof maxit === "undefined") maxit = 1000
+    const gradient = x => grad(fg, x)
     x0 = numeric.clone(x0)
     const n = x0.length
     let [f0, g0] = fg(x0, true)
@@ -931,121 +930,6 @@ function uncmin(fg, x0, tol, maxit, callback, options) {
         f0 = f1
         g0 = g1
         it++
-    }
-    return {
-        solution: x0,
-        f: f0,
-        gradient: g0,
-        invHessian: H1,
-        iterations: it,
-        message: msg
-    }
-}
-
-// From numeric-1.2.6.js. Modified to allow f to supply the gradient directly.
-//
-// Returns: a local minimum of a function f(x) whose gradient is g(x).
-// Requires: fg(x) or fg(x, false) must return just f(x),
-//   fg(x, true) must return [ f(x), (grad f)(x)] or [f(x0), undefined]. In the former case,
-//   (grad f) must return an array whose length is the same as x. In the latter
-//   case, a numeric gradient is computed using f, probably more expensively.
-function uncmin(fg, x0, tol, maxit, callback, options) {
-    const grad = numeric.gradient
-    if (typeof options === "undefined") {
-        options = {}
-    }
-    if (typeof tol === "undefined") {
-        tol = 1e-8
-    }
-
-    function gradient(x) {
-        return grad(fg, x)
-    }
-    if (typeof maxit === "undefined") maxit = 1000
-    x0 = numeric.clone(x0)
-    const n = x0.length
-    let [f0, g0] = fg(x0, true)
-    let f1, g1, df0
-    if (isNaN(f0))
-        throw new Error('uncmin: f(x0) is a NaN!')
-    if (g0 === undefined) g0 = gradient(x0)
-    const max = Math.max,
-        norm2 = numeric.norm2
-    tol = max(tol, numeric.epsilon)
-    let step, H1 = options.Hinv || numeric.identity(n)
-    const dot = numeric.dot,
-        inv = numeric.inv,
-        sub = numeric.sub,
-        add = numeric.add,
-        ten = numeric.tensor,
-        div = numeric.div,
-        mul = numeric.mul
-    const all = numeric.all,
-        isfinite = numeric.isFinite,
-        neg = numeric.neg
-    let it = 0,
-        i, s, x1, y, Hy, Hs, ys, i0, t, nstep, t1, t2
-    let msg = ""
-    while (it < maxit) {
-        if (typeof callback === FUNCTION) {
-            if (callback(it, x0, f0, g0, H1)) {
-                msg = "Callback returned true"
-                break
-            }
-        }
-        if (!all(isfinite(g0))) {
-            msg = "Gradient has Infinity or NaN"
-            break
-        }
-        step = neg(dot(H1, g0))
-        if (!all(isfinite(step))) {
-            msg = "Search direction has Infinity or NaN"
-            break
-        }
-        nstep = norm2(step)
-        if (nstep < tol) {
-            msg = "Newton step smaller than tol"
-            break
-        }
-        t = 1
-        df0 = dot(g0, step)
-        // line search
-        x1 = x0
-        while (it < maxit) {
-            if (t * nstep < tol) {
-                break
-            }
-            s = mul(step, t)
-            x1 = add(x0, s)
-            f1 = fg(x1)
-            if (f1 - f0 >= 0.1 * t * df0 || isNaN(f1)) {
-                t *= 0.5
-                ++it
-                continue
-            }
-            break
-        }
-        if (t * nstep < tol) {
-            msg = "Line search step size smaller than tol"
-            break
-        }
-        if (it === maxit) {
-            msg = "maxit reached during line search"
-            break
-        }
-        [f1, g1] = fg(x1, true)
-        if (g1 === undefined) g1 = gradient(x1)
-        y = sub(g1, g0)
-        ys = dot(y, s)
-        Hy = dot(H1, y)
-        H1 = sub(add(H1,
-                mul((ys + dot(y, Hy)) / (ys * ys),
-                    ten(s, s))),
-            div(add(ten(Hy, s), ten(s, Hy)), ys))
-        x0 = x1
-        f0 = f1
-        g0 = g1
-        ++it
     }
     return {
         solution: x0,
