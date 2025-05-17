371d355742fdb2e4e16aead0b4fac0d7429e8435
Andrew Myers <andru@cs.cornell.edu>
2025-05-17 10:41:37 -0400

    More progress.

--- editor.js
+++ editor.js
@@ -1,5 +1,10 @@
 (() => {
 
+const evaluate = Constrain.evaluate
+const sqdist = Constrain.sqdist
+const arrows = Constrain.arrows
+const PI2 = Math.PI * 2
+
 class EditableFigure extends Constrain.Figure {
     constructor(canvas) {
         super(canvas)
@@ -26,16 +31,146 @@ class EditableFigure extends Constrain.Figure {
     }
 }
 
-const sqdist = Constrain.sqdist
+
+class Selectable {
+    constructor(editable, aligns) {
+        this.aligns = aligns
+        this.editable = editable
+        this.dragging = false
+    }
+    render(ctx) {
+        console.error("unimplemented")
+    }
+    xy() {
+        console.error("unimplemented")
+    }
+    selectablePoints() {
+        return []
+    }
+    setDragging(dragging) {
+        this.dragging = dragging
+    }
+}
+
+const selectionDist2 = 16 // minimum squared distance for selectability
+
+class SelectablePoint extends Selectable {
+    constructor(editable, ex, ey, aligns) {
+        super(editable, aligns)
+        this.expr = [ex, ey]
+    }
+    x() {
+        return this.expr[0]
+    }
+    y() {
+        return this.expr[1]
+    }
+    xy() {
+        return this.expr
+    }
+    // squared distance to (mx, my) if it is close, or null otherwise
+    distanceTo(mx, my) {
+        const [vx, vy] = evaluate(this.expr)
+        const d = sqdist(mx - vx, my - vy)
+        if (d > selectionDist2) return null
+        return d
+    }
+    render(ctx) {
+        const [x, y] = evaluate(this.expr)
+        const editable = this.editable
+        ctx.beginPath()
+        ctx.fillStyle = "black"
+        ctx.beginPath()
+        ctx.arc(x, y, 2.5, 0, PI2)
+        ctx.fill()
+        ctx.beginPath()
+        ctx.arc(x, y, 2, 0, PI2)
+        const j = this.aligns[0], k = this.aligns[1]
+        if (this.selected) {
+            ctx.fillStyle = "magenta"
+        } else if (j >= 0 && editable.constraints[j] && editable.constraints[j].size > 0 ||
+                    k >= 0 && editable.constraints[k] && editable.constraints[k].size > 0) {
+            ctx.fillStyle = "orange"
+        } else {
+            ctx.fillStyle = "yellow"
+        }
+        ctx.fill()
+    }
+    selectablePoints() {
+        return [this]
+    }
+}
+
+function distToOrthLine(mx, my, x1, x2, y) {
+    const dy = my - y, dysq = dy*dy,
+          dx1 = mx - x1, dx2 = mx - x2,
+          dxsq = Math.sign(x1 - mx) != Math.sign(x2 - mx)
+                ? 0
+                : Math.min(dx1*dx1, dx2*dx2)
+    return dxsq + dysq
+}
+
+function hwline(ctx, x1, y1, x2, y2) {
+    ctx.beginPath()
+    ctx.lineWidth = 2
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
+    ctx.stroke()
+    arrows.arrow(ctx, x1, y1, x2, y2, 8)
+    arrows.arrow(ctx, x2, y2, x1, y1, 8)
+}
+
+class SelectableHorzLine extends Selectable {
+    constructor(editable, ex1, ex2, ey) {
+        super(editable)
+        this.ex1 = ex1
+        this.ex2 = ex2
+        this.ey = ey
+    }
+    distanceTo(mx, my) {
+        const [vx1, vx2, vy] = evaluate([this.ex1, this.ex2, this.ey])
+        const d2 = distToOrthLine(mx, my, vx1, vx2, vy)
+        if (d2 > selectionDist2) return null
+        return d2
+    }
+    render(ctx) {
+        ctx.fillStyle = this.dragging ? "magenta" :
+            this.constrained ? "orange" : "yellow"
+        const [vx1, vx2, vy] = evaluate([this.ex1, this.ex2, this.ey])
+        hwline(ctx, vx1, vy, vx2, vy)
+    }
+}
+
+class SelectableVertLine extends Selectable {
+    constructor(editable, ex, ey1, ey2) {
+        super(editable)
+        this.ex = ex
+        this.ey1 = ey1
+        this.ey2 = ey2
+    }
+    distanceTo(mx, my) {
+        const [vx, vy1, vy2] = evaluate([this.ex, this.ey1, this.ey2])
+        const d2 = distToOrthLine(my, mx, vy1, vy2, vx)
+        if (d2 > selectionDist2) return null
+        return d2
+    }
+    render(ctx) {
+        ctx.fillStyle = this.dragging ? "magenta" :
+            this.constrained ? "orange" : "yellow"
+        const [vx, vy1, vy2] = evaluate([this.ex, this.ey1, this.ey2])
+        hwline(ctx, vx, vy1, vx, vy2)
+    }
+}
 
 class EditableGraphic extends Constrain.Control {
     constructor(figure, graphic) {
         super(figure, graphic, 4)
         this.graphic = graphic
         this.isActive = false
-        this.dragging = false
+        this.dragStart = null // [mx, my] if dragging
         this.min_align_dist = 4
-        this.selectionDist2 = 16 // minimum squared distance for selectability
+        this.selectionDist2 = 16
+        this.selectedControl = null
         this.constraints = [] // constraints associated with each coordinate: x0, xc, x1, y0, yc, y1
     }
     active() {
@@ -44,7 +179,7 @@ class EditableGraphic extends Constrain.Control {
     mousedown(x, y, e) {
         if (!this.active() && !this.inBounds(x, y)) return false
         if (this.active() && !this.inBounds(x, y)) {
-            this.selectedIndex = -1
+            this.selectedControl = null
             this.isActive = false
             this.figure.delayedRender()
             return false
@@ -52,38 +187,52 @@ class EditableGraphic extends Constrain.Control {
         const figure = this.figure
         this.isActive = true
         this.dragging = true
-        const {evaluated, expr, aligns} = this.selectablePts()
-        let bestd = this.selectionDist2 + 1, besti = -1
-        for (let i = 0; i < evaluated.length; i++) {
-            const [x2, y2] = evaluated[i]
-            const d = sqdist(x - x2, y - y2)
-            if (d < bestd && d < this.selectionDist2) {
+        const selectables = this.selectableControls()
+        let bestd = this.selectionDist2 + 1, bestControl = null
+        for (let i = 0; i < selectables.length; i++) {
+            const s = selectables[i],
+                  d = s.distanceTo(x, y)
+            if (d != null && d < bestd) {
                 bestd = d
-                besti = i
+                bestControl = s
             }
         }
-        if (bestd <= this.selectionDist2) {
-            this.selectedIndex = besti
-            this.updateConstraints(expr[besti], aligns[besti], x, y)
+        if (bestControl) {
+            this.selectedControl = bestControl
+            bestControl.setDragging(true)
+            this.dragStart = [x,y]
+            this.updateConstraints(bestControl, x, y)
             figure.setFocus(this)
         }
         figure.delayedRender()
         return true
     }
-    updateConstraints(expr, aligns, x, y) {
-        if (aligns[0] >= 0) {
-            const xi = aligns[0]
-            this.disableConstraints(xi)
-            const s = this.constraints[xi] = new Set()
-            if (x !== undefined) s.add(figure.equal(expr[0], x))
-            this.enableConstraints(xi)
+    removeConstraints(control) {
+        const aligns = control.aligns
+        console.log('trying to remove constraints on ', control)
+        for (c of aligns) {
+            console.log('trying to remove constraint ', c)
+            if (c) this.disableConstraints(c)
         }
-        if (aligns[1] >= 0) {
-            const yi = aligns[1]
-            this.disableConstraints(yi)
-            const s = this.constraints[yi] = new Set()
-            if (y !== undefined) s.add(figure.equal(expr[1], y))
-            this.enableConstraints(yi)
+    }
+    updateConstraints(control, mx, my) {
+        const aligns = control.aligns
+        console.log("updating constraints", aligns)
+        if (aligns && aligns[0]) {
+            const xv = aligns[0]
+            this.disableConstraints(xv)
+            const s = this.constraints[xv] = new Set()
+            const x = control.x()
+            if (x) s.add(figure.equal(mx, x))
+            this.enableConstraints(xv)
+        }
+        if (aligns && aligns[1]) {
+            const yv = aligns[1]
+            this.disableConstraints(yv)
+            const s = this.constraints[yv] = new Set()
+            const y = control.y()
+            if (y) s.add(figure.equal(my, y))
+            this.enableConstraints(yv)
         }
         this.figure.discardSolverHints()
     }
@@ -106,12 +255,12 @@ class EditableGraphic extends Constrain.Control {
         if (this.figure.getFocus() === this) {
             this.figure.loseFocus()
             this.figure.delayedRender()
-            if (this.dragging) {
+            if (this.dragStart) {
                 if (this.bestHorizontal || this.bestVertical) {
-                    const {evaluated, expr, aligns} = this.selectablePts()
-                    const i = this.selectedIndex
+                    const selectables = this.selectableControls()
+                    const control = this.selectedControl
                     if (this.bestHorizontal) {
-                        const j = aligns[i][0]
+                        const j = control.aligns[0]
                         if (j >= 0) {
                             this.disableConstraints(j)
                             this.constraints[j] = new Set()
@@ -120,7 +269,7 @@ class EditableGraphic extends Constrain.Control {
                         }
                     }
                     if (this.bestVertical) {
-                        const j = aligns[i][1]
+                        const j = control.aligns[1]
                         if (j >= 0) {
                             this.disableConstraints(j)
                             this.constraints[j] = new Set()
@@ -130,78 +279,91 @@ class EditableGraphic extends Constrain.Control {
                     }
                 }
                 this.figure.discardSolverHints()
-                this.dragging = false
+                this.dragStart = null
+                if (this.selectedControl) {
+                    this.selectedControl.setDragging(false)
+                    this.selectedControl = false
+                }
             }
         }
     }
     mousemove(x, y, e) {
-        const ctx = figure.ctx
-        ctx.fillStyle = '#0ff';
-        ctx.beginPath()
-        ctx.arc(x, y, 1, 0, 2*Math.PI)
-        ctx.fill()
-
-        if (this.selectedIndex >= 0) {
-            const i = this.selectedIndex
-            const selectables =  this.selectablePts()
-            const {evaluated, expr, aligns} = selectables
-            this.updateConstraints(expr[i], aligns[i], x, y)
-            this.findAlignments(x,y,i,selectables)
+        if (this.selectedControl) {
+            const c = this.selectedControl
+            this.updateConstraints(c, x, y)
+            this.findAlignments(x,y,c.x(), c.y())
             this.figure.delayedRender()
         }
     }
     keydown(e) {
         if (e.key === ' ' && this.isActive) {
-            if (this.selectedIndex >= 0) {
-                const {evaluated, expr, aligns} = this.selectablePts()
-                const i = this.selectedIndex
-                this.updateConstraints(expr[i], aligns[i])
-                this.selectedIndex = -1
+            if (this.selectedControl) {
+                const c = this.selectedControl
+                this.removeConstraints(c)
+                this.selectedControl = null
                 this.figure.delayedRender()
             }
         }
     }
 
+    alignablePoints() {
+        const result = []
+        for (const control of this.selectableControls()) {
+            for (const pt of control.selectablePoints()) {
+                result.push(pt.xy())
+            }
+        }
+        return result
+    }
+
     // Return an array of selectable points both in unevaluated and evaluated form
     // as an object o = {evaluated, expr, aligns}
     // o.aligns is a 2-element array specifying what coordinates each of the selectable points
-    // aligns to, as an index between 0 and 5, or -1 if no alignment on that axis should
+    // aligns to, as an index between 0 and 5, or null if no alignment on that axis should
     // occur.
-    selectablePts() {
-        const g = this.graphic
+    selectableControls() {
+        const g = this.graphic, figure = this.figure
         const coords = [g.x0(), g.x(), g.x1(), g.y0(), g.y(), g.y1()]
-        const values = Constrain.evaluate(coords)
-        const evaluated = [], expr = [], aligns = []
+        const values = evaluate(coords)
+        const selectables = []
+        // const evaluated = [], expr = [], aligns = []
         for (let i = 0; i < 9; i++) {
             const j = i%3, k = 3 + Math.floor(i/3)
-            expr[i] = [coords[j], coords[k]]
-            evaluated[i] = [values[j], values[k]]
-            aligns[i] = [-1, -1]
-            if (i != 1 && i != 7) aligns[i][0] = j
-            if (i != 3 && i != 5) aligns[i][1] = k
+            const aligns = [null, null]
+            if (i != 1 && i != 7) aligns[0] = coords[j]
+            if (i != 3 && i != 5) aligns[1] = coords[k]
+            selectables.push(new SelectablePoint(this, coords[j], coords[k], aligns))
         }
-        return { evaluated, expr, aligns }
+        selectables.push(new SelectableHorzLine(this, coords[0], coords[2], figure.average(coords[3], coords[4])))
+        selectables.push(new SelectableVertLine(this, figure.average(coords[0], coords[1]), coords[3], coords[5]))
+        return selectables
     }
-    findAlignments(x,y,i,selectable) {
-        const {evaluated, expr, aligns} = selectable
-        const [xi, yi] = aligns[i]
+
+    // Find the best alignments of the mouse position (mx, my), against things
+    // in the figure. xs and ys are the expressions representing the control
+    // point whose position is being adjusted.
+    // Effect: Sets the fields bestHorizontal, bestVertical to
+    // either null (if no suitable alignment is found) or an object { dist,
+    // pos, expr }. Each of xs and ys may be null to indicate no alignment
+    // should be done on that axis.
+    findAlignments(mx, my, xs, ys) {
         const min_align_dist = this.min_align_dist
         let bestHorizontal = null, bestVertical = null
         for (const io of this.figure.interactives) {
             if (io === this) continue
-            if (io.selectablePts) {
-                const selectable2 = io.selectablePts()
-                for (let j = 0; j < selectable2.evaluated.length; j++) {
-                    const [x2, y2] =  selectable2.evaluated[j]
-                    const dx = Math.abs(x2 - x) 
-                    const dy = Math.abs(y2 - y) 
-                    if (dx < min_align_dist &&
+            if (io.alignablePoints) {
+                const points = io.alignablePoints()
+                for (const p of points) {
+                    const [x2, y2] =  evaluate(p),
+                          dx = Math.abs(x2 - mx),
+                          dy = Math.abs(y2 - my) 
+                    if (xs && dx < min_align_dist &&
                         (bestHorizontal === null || dx < bestHorizontal.dist)) {
-                        bestHorizontal = { dist : dx, pos : x2, expr : selectable2.expr[j][0] }
+                        bestHorizontal = { dist : dx, pos : x2, expr : p[0] }
                     }
-                    if (dy < min_align_dist &&
-                        (bestVertical === null || Math.abs(y2 - y) < bestVertical.dist)) {
-                        bestVertical = { dist : dy, pos : y2, expr : selectable2.expr[j][1] }
+                    if (ys && dy < min_align_dist &&
+                        (bestVertical === null || dy < bestVertical.dist)) {
+                        bestVertical = { dist : dy, pos : y2, expr : p[1] }
                     }
                 }
             }
@@ -214,8 +376,8 @@ class EditableGraphic extends Constrain.Control {
         ctx.save()
         ctx.strokeStyle = "yellow"
         ctx.lineWidth = 1.5
-        const [x0, x1, y0, y1] = Constrain.evaluate([this.graphic.x0(), this.graphic.x1(), this.graphic.y0(), this.graphic.y1()])
-        const [xc, yc] = Constrain.evaluate([this.graphic.x(), this.graphic.y()])
+        const [x0, x1, y0, y1] = evaluate([this.graphic.x0(), this.graphic.x1(), this.graphic.y0(), this.graphic.y1()])
+        const [xc, yc] = evaluate([this.graphic.x(), this.graphic.y()])
         ctx.beginPath()
         ctx.moveTo(x0, y0)
         ctx.lineTo(x1, y0)
@@ -223,51 +385,12 @@ class EditableGraphic extends Constrain.Control {
         ctx.lineTo(x0, y1)
         ctx.closePath()
         ctx.stroke()
-        const selectable = this.selectablePts()
-        const pi2 = 2 * Math.PI
-        ctx.fillStyle = "black"
-        for (const pt of selectable.evaluated) {
-            const [x, y] = pt
-            ctx.beginPath()
-            ctx.arc(x, y, 2.5, 0, pi2)
-            ctx.fill()
-        }
-        for (let i = 0; i < selectable.evaluated.length; i++) {
-            const [x, y] = selectable.evaluated[i]
-            ctx.beginPath()
-            ctx.arc(x, y, 2, 0, pi2)
-            const j = selectable.aligns[i][0], k = selectable.aligns[i][1]
-            ctx.fillStyle = "green"
-            if (i === this.selectedIndex) {
-                ctx.fillStyle = "magenta"
-            } else if (j >= 0 && this.constraints[j] && this.constraints[j].size > 0 ||
-                       k >= 0 && this.constraints[k] && this.constraints[k].size > 0) {
-                ctx.fillStyle = "orange"
-            } else {
-                ctx.fillStyle = "yellow"
-            }
-            ctx.fill()
+        const selectable = this.selectableControls()
+        for (const control of selectable) {
+            control.render(ctx)
         }
 
-        function hwline(x1, y1, x2, y2) {
-            ctx.moveTo(x1, y1)
-            ctx.lineTo(x2, y2)
-            ctx.fillStyle = "yellow"
-            ctx.stroke()
-            Constrain.arrows.arrow(ctx, x1, y1, x2, y2, 8)
-            Constrain.arrows.arrow(ctx, x2, y2, x1, y1, 8)
-
-        }
-
-        ctx.beginPath()
-        ctx.lineWidth = 2
-        const yh = y0*0.75 + y1*0.25
-        hwline(x0, yh, x1, yh)
-
-        const xh = x0*0.75 + x1*0.25
-        hwline(xh, y0, xh, y1)
-
-        if (this.dragging) {
+        if (this.dragStart) {
             ctx.strokeStyle = "magenta"
             ctx.setLineDash([5,3])
             ctx.lineWidth = 0.75
